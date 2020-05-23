054a7a469cfb065edfd2cf11a59e4a419ee235a9
moonjason <jasonmoonjm@gmail.com>
2020-05-22 23:55:41 -0700

    import firebase auth methods

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ ReactDOM.render(
   , document.getElementById('root')
 );
 
+// Firebase is not injected to the React component tree via the React Context AP
+// Now for every component interested in Firebase, may access to the Firebase instance with FirebaseContext.Consumer component
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
