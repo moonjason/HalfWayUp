6bc1d6b2abcaf245c3b1325464a514136ffdc353
moonjason <jasonmoonjm@gmail.com>
2020-05-22 22:29:52 -0700

    component directory, .env

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
