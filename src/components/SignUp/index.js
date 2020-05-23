a30eb0ab7d6e278561b1c7d695fe7133450ee39e
moonjason <jasonmoonjm@gmail.com>
2020-05-23 01:24:16 -0700

    centered box signup page

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const SignUp = () => {
     return (
-        <div className="signup-container">
-            <h1>SignUp</h1>
-        </div>
+            <div className="signup-container">
+                <Row noGutters={true}>
+                    <Col md={4} className="d-md-block d-sm-none"></Col>
+                    <Col className="center-column" md={4}>
+                        <div className="signup-box">
+                        </div>
+                    </Col>
+                    <Col md={4} className="d-md-block d-sm-none"></Col>
+                </Row>
+            </div>
     );
 }
 
