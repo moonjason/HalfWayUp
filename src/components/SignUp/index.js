102786009c64c5c788da7e628e1bba1f72cf753d
moonjason <jasonmoonjm@gmail.com>
2020-05-23 18:09:34 -0700

    rearrange signup component, new font

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const SignUp = () => {
     return (
-            <div className="signup-container">
-                <Row noGutters={true}>
-                    <Col md={4} className="d-md-block d-sm-none"></Col>
-                    <Col className="center-column" md={4}>
-                        <div className="signup-box">
-                            <div className="signup-form">
-                                <h1 className="signup-title">Sign Up</h1>
+            <div>
+                <div className="app-logo">
+                    <h1>HalfwayUp</h1>
+                </div>
+                <div className="signup-container">
+                    <Row noGutters={true}>
+                        <Col md={4} className="d-md-block d-sm-none"></Col>
+                        <Col className="center-column" md={4}>
+                            <div className="signup-box">
+                                <div className="signup-form">
+                                    <h1 className="signup-title">Sign Up</h1>
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                    <Col md={4} className="d-md-block d-sm-none"></Col>
-                </Row>
+                        </Col>
+                        <Col md={4} className="d-md-block d-sm-none"></Col>
+                    </Row>
+                </div>
             </div>
     );
 }
