9fa862e06eed1dc6bd3a4b77833f7a3b767aeb10
moonjason <jasonmoonjm@gmail.com>
2020-05-24 18:04:05 -0700

    google button highlight

--- src/components/SignInGoogleBase/SignInGoogleBase.js
+++ src/components/SignInGoogleBase/SignInGoogleBase.js
@@ -2,8 +2,13 @@ import React, {Component} from 'react';
 
 import { withFirebase } from '../Firebase';
 
+
+
 class SignInGoogleBase extends Component {
-    state = {  error : null };
+    state = { 
+       error : null,
+       src:"/images/btn_google.png" 
+    };
 
     onSubmit = event => {
           this.props.firebase.doSignInWithGoogle()
@@ -17,12 +22,20 @@ class SignInGoogleBase extends Component {
      
         event.preventDefault();
     };
+
+    onMouseOver = () => {
+      this.setState({src: '/images/btn_google_hover.png'})
+    }
+    onMouseOut = () => {
+      this.setState({src: '/images/btn_google.png'})
+    }
+
     render() {
         const { error } = this.state;
         
         return (
             <form className="google-signin" onSubmit={this.onSubmit}>
-              <button type="submit"><img src="/images/btn_google.png"></img></button>
+              <button type="submit"><img className="google-img" src={this.state.src} onMouseOver={() => this.onMouseOver()} onMouseOut={() => this.onMouseOut()}></img></button>
           
               {error && <p className="google-error">{error.message}</p>}
             </form>
